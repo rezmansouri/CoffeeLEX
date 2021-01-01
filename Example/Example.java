be2f09c2a46435e60d5ee1d64aa0e76f7cf936b0
Reza Mansouri <mrmansourireza@gmail.com>
2021-01-01 18:19:24 +0330

    Add Examples

---
+++ Example/Example.java
@@ -0,0 +1,12 @@
+class Example {
+    public static void main(String[] args) {
+        int integerVariable = 100;
+        int minusIntegerVariable = -100;
+        int result = integerVariable - minusIntegerVariable;
+        for (int i=0; i<result; i++) {
+            System.out.println(i);
+        }
+        String x = "hello";
+        //This is a Comment
+    }
+}
